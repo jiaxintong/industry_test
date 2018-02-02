2413c96a8eb41c1d5e0183d219ec68e41e67f960
jiaxintong <1184713738@qq.com>
2018-02-02 04:24:29 -0500

    102 packets test

--- hello_scapy.py
+++ hello_scapy.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
 	packet_format = packet_format[:-1] + ")"
         print packet_format
     eval("sr("+packet_format+")")
-    wrpcap("modbus.pcap", pkts)
+    wrpcap("modbus_2.pcap", pkts)
